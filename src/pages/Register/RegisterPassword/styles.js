eaa08727732a66a0fdadddf5c66d7d4d7eaabf9d
DavidBitencourt <38070806+DavidBitencourt@users.noreply.github.com>
2020-04-10 07:13:39 -0300

    register user pages

---
+++ src/pages/Register/RegisterPassword/styles.js
@@ -0,0 +1,33 @@
+import styled from "styled-components/native";
+
+export const BoxConteinerStyled = styled.View`
+  width: 100%;
+  height: 100%;
+  align-items: center;
+`;
+
+export const BoxInputStyled = styled.View`
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  margin-top: 37px;
+`;
+
+export const BoxButtonsStyled = styled.View`
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  margin-top: 95%;
+`;
+
+export const TextStyled = styled.Text`
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  font-weight: 300;
+  font-size: 23px;
+  line-height: 30px;
+  color: #eb8a75;
+  text-align: center;
+  margin-top: 50px;
+`;
